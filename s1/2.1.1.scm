aba80725ab1fead6b42230b74e210170a74a2e82
test <test@email.arg>
2022-11-16 22:58:33 +0100

    First excercice

---
+++ s1/2.1.1.scm
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.1.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; find out whether DrScheme has operations
+;; for squaring a number; for computing the
+;; sine of an angle; and for determining the
+;; maximum of two numbers.
+(sqr 10)
+(sin 0)
+(max 5 4)
